5247f7f44f8b353656fdee1fb5a20124072dcc5e
dfavo <favorskyd@gmail.com>
2020-04-07 22:00:25 -0700

    Did Login page and Text box component

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import classes from './FinishedQuiz.module.css';
 import Button from "../UI/Button/Button";
+import {Link} from 'react-router-dom';
+
+
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
         if (props.results[key] === 'success') {
@@ -47,7 +50,10 @@ const FinishedQuiz = props => {
 
            <div>
                <Button onClick={props.onRetry} type="primary">Repeat</Button>
-               <Button  type="success">Go to the test list</Button>
+               <Link to="/">
+                   <Button  type="success">Go to the test list</Button>
+               </Link>
+
            </div>
        </div>
    )
